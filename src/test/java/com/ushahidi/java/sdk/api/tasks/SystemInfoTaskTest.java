dfaca6c918bb90907e661f014e7c062e57199f2d
eyedol <addhen@linuxmail.org>
2012-10-22 09:06:15 +0000

    Add system info task test

---
+++ src/test/java/com/ushahidi/java/sdk/api/tasks/SystemInfoTaskTest.java
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ ** Copyright (c) 2010 - 2012 Ushahidi Inc
+ ** All rights reserved
+ ** Contact: team@ushahidi.com
+ ** Website: http://www.ushahidi.com
+ **
+ ** GNU Lesser General Public License Usage
+ ** This file may be used under the terms of the GNU Lesser
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation and appearing in the file LICENSE.LGPL included in the
+ ** packaging of this file. Please review the following information to
+ ** ensure the GNU Lesser General Public License version 3 requirements
+ ** will be met: http://www.gnu.org/licenses/lgpl.html.
+ **
+ **
+ ** If you have questions regarding the use of this file, please contact
+ ** Ushahidi developers at team@ushahidi.com.
+ **
+ *****************************************************************************/
+package com.ushahidi.java.sdk.api.tasks;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The SystemInfoTask to be tested
+ * 
+ * TODO: test the various methods as defined in the SystemInfoTask class
+ * 
+ * @author eyedol
+ *
+ */
+public class SystemInfoTaskTest extends BaseTaskTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
