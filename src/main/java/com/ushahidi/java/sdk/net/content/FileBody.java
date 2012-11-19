eed9e2a377282c411af5b0f988ac1af891e74c52
Henry Addo <addhen@linuxmail.org>
2012-11-19 15:13:47 +0000

    Remove unused imported class

--- src/main/java/com/ushahidi/java/sdk/net/content/FileBody.java
+++ src/main/java/com/ushahidi/java/sdk/net/content/FileBody.java
@@ -24,7 +24,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * Encapsulates the file to be uploaded
