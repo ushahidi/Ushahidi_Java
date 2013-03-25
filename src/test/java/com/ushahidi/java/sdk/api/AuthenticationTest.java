ebe30bc5ee13c132cdfbf0525d403a85d005577a
mrl <mrl@geothings.tw>
2013-03-25 10:38:46 +0800

    added test for authentication

---
+++ src/test/java/com/ushahidi/java/sdk/api/AuthenticationTest.java
@@ -0,0 +1,62 @@
+package com.ushahidi.java.sdk.api;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.ushahidi.java.sdk.UshahidiException;
+import com.ushahidi.java.sdk.api.json.Reports;
+import com.ushahidi.java.sdk.api.tasks.BaseTask;
+import com.ushahidi.java.sdk.api.tasks.BaseTaskTest;
+import com.ushahidi.java.sdk.api.tasks.ReportsAdminTask;
+
+public class AuthenticationTest extends BaseTaskTest {
+
+	private ReportsAdminTask task;
+
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		task = factory.createReportsAdminTask();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+		task = null;
+	}
+
+	/**
+	 * Test method for authenticating with incorrect username & password
+	 */
+	@Test
+	public void testIncorrectCredential() {
+		task.setAuthentication(USERNAME, PASSWORD+"FAIL");
+		try {
+			task.verified();
+		} catch (UshahidiException ue) {
+			String json = ue.getCause().getMessage();
+			Reports r = BaseTask.fromString(json, Reports.class);
+			Assert.assertEquals(r.getErrorCode(), 5);
+			return;
+		} catch (Exception e) {
+			Assert.fail();
+		}
+		Assert.fail();
+	}
+
+	/**
+	 * Test method for authenticating with correct username & password
+	 */
+	@Test
+	public void testCorrectCredential() {
+		try {
+			task.setAuthentication(USERNAME, PASSWORD);
+			task.verified();
+		} catch (Exception e) {
+			Assert.fail();
+		}
+	}
+
+}
